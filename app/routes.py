df0b4c9f3c629e3119528e5b914ab82680dace5d
Teemu Kilpelainen <teemu.k.kilpelainen@gmail.com>
2020-01-08 19:07:06 +0200

    fixed misleading title

--- app/routes.py
+++ app/routes.py
@@ -4,4 +4,4 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html', title='Breadth first search')
+    return render_template('index.html', title='Dijkstra')
